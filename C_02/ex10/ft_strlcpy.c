19bcb1e93aa4885cee7281b52ce4ad6366d84c71
Stepan Skopek <sskopek@c2r6s3.42prague.com>
2024-06-10 18:09:22 +0200

    chore(C02): idk what was wrong

--- C_02/ex10/ft_strlcpy.c
+++ C_02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: sskopek <sskopek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 19:48:38 by sskopek           #+#    #+#             */
-/*   Updated: 2024/06/09 00:42:48 by sskopek          ###   ########.fr       */
+/*   Updated: 2024/06/10 18:08:20 by sskopek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,17 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (size < 1)
+		return (src_len);
 	while (src[i] != '\0' && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	src_len = 0;
-	while (src[src_len] != '\0')
-		src_len++;
 	return (src_len);
 }
 
