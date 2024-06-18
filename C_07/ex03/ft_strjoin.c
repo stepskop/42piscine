8d5889c3739c3700cff1bf7cb5a30e2d90596b66
Stepan Skopek <sskopek@c2r6s3.42prague.com>
2024-06-18 21:28:42 +0200

    chore(C07/ex03): fixed?

--- C_07/ex03/ft_strjoin.c
+++ C_07/ex03/ft_strjoin.c
@@ -6,12 +6,11 @@
 /*   By: sskopek <sskopek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 12:01:25 by sskopek           #+#    #+#             */
-/*   Updated: 2024/06/18 18:46:55 by sskopek          ###   ########.fr       */
+/*   Updated: 2024/06/18 21:28:26 by sskopek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
@@ -23,65 +22,63 @@ int	ft_strlen(char *str)
 	return (count);
 }
 
-char	*ft_fillbuffer(char **strs, char *buffer, char *sep, int b_len)
+char	*create_buffer(int size, char **strs, char *sep)
 {
+	char	*buffer;
+	int		len;
 	int		i;
-	int		j;
-	int		strs_item;
 
 	i = 0;
-	strs_item = 0;
-	while (i < b_len)
+	len = 0;
+	while (i < size)
 	{
-		j = 0;
-		while (strs[strs_item][j] && i + j < b_len)
-		{
-			buffer[i + j] = strs[strs_item][j];
-			j++;
-		}
-		i += j;
-		j = 0;
-		while (sep[j] && i + j < b_len)
-		{
-			buffer[i + j] = sep[j];
-			j++;
-		}
-		i += j;
-		strs_item++;
+		len += ft_strlen(strs[i]);
+		i++;
 	}
+	len += ft_strlen(sep) * (size - 1);
+	if (size <= 0)
+		len = 1;
+	buffer = (char *)malloc(sizeof(char) * len);
 	return (buffer);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	char	*buffer;
-	int		final_len;
-	int		stra_len;
+	char	*str;
 	int		i;
+	int		j;
+	int		f;
 
-	stra_len = 0;
-	i = 0;
-	while (i < size)
+	str = create_buffer(size, strs, sep);
+	i = -1;
+	f = 0;
+	while (++i < size)
 	{
-		stra_len += ft_strlen(strs[i]);
-		i++;
+		j = 0;
+		while (strs[i][j])
+		{
+			str[f++] = strs[i][j];
+			j++;
+		}
+		j = 0;
+		while (sep[j] && i < (size - 1))
+		{
+			str[f++] = sep[j];
+			j++;
+		}
 	}
-	final_len = stra_len + ((size - 1) * ft_strlen(sep));
-	buffer = malloc(sizeof(char) * (final_len + 1));
-	return (ft_fillbuffer(strs, buffer, sep, final_len));
+	str[f] = '\0';
+	return (str);
 }
 
+// #include <stdio.h>
 // int main(void)
 // {
-// 	char *text[] = {"", "adsad"} ;
+// 	char *text[] = {"Mirek", "je", "frajer"} ;
 // 	char *str;
-// 	int word_count = 4;
-// 	str = ft_strjoin(0, text, " -- ");
-// 	for (int i = 0; i < 1; i++)
-// 	{
-// 		str = ft_strjoin(2, text, "");
-// 		printf("%s", str);	
-// 	}
+// 	int word_count = 3;
+// 	str = ft_strjoin(word_count, text, " ");
+// 	printf("%s", str);
 // 	free(str);
 // 	return 0;
 // }
