b29b0b2f5e961f6f7f35a974d5a36a1afa370aab
Stepan Skopek <sskopek@c2r6s3.42prague.com>
2024-06-24 13:21:47 +0200

    fix(BSQ/parse): fix unclosed fd's and malloc

--- BSQ/srcs/parse/read.c
+++ BSQ/srcs/parse/read.c
@@ -6,7 +6,7 @@
 /*   By: sskopek <sskopek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 11:55:00 by sskopek           #+#    #+#             */
-/*   Updated: 2024/06/24 11:55:18 by sskopek          ###   ########.fr       */
+/*   Updated: 2024/06/24 13:13:44 by sskopek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,15 @@ char	*read_file(char *file)
 	{
 		while (read(file_fd, &c, 1))
 			f_size++;
+		close(file_fd);
 	}
-	buffer = malloc(sizeof(char) * f_size);
-	close(file_fd);
+	buffer = malloc(sizeof(char) * (f_size + 1));
 	file_fd = open(file, O_RDONLY);
 	if (file_fd != -1)
+	{
 		bytes_read = read(file_fd, buffer, f_size);
+		close(file_fd);
+	}
 	buffer[bytes_read] = '\0';
 	return (buffer);
 }
